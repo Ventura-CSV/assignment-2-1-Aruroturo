cb8a4504cb7b87f63f6d87298cae7174eb87e4f2
Aruroturo <arturo.alfaro3@my.vcccd.edu>
2024-06-19 16:31:25 -0700

    actual one?

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 def main():
    # Comlete your code here
    # Use m_perc=3 , f_perc, nb_perc for your results
+   #input number of males
     men_num = int(input("Enter The Number of Male Students: "))
-    
+    # input number of females
     fem_num = int(input("Enter The Number of Female Students: "))
-    
+    #input number of non binary
     nonb_num = int(input(" Enter The Number of Non-binary Students: "))
-    
+    #Computer caculates the total number of all students
     total_peeps = men_num + fem_num + nonb_num
-   
+   # This is the percentage caculation of the males
     m_perc = (men_num / total_peeps) * 100
-    
+    # This is the percentage calculation of the females
     f_perc = (fem_num / total_peeps) * 100
-
+# this is the percentage caculation of the non binary
     nb_perc = (nonb_num / total_peeps) * 100
-
+# This entire section is the output of our information
     print(f"The total Number of Students: t\t{total_peeps}")
 
     print(f"The Number of Males, Femals and Non-binary: \t{men_num} \t{fem_num} \t{nonb_num}")
