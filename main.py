1d6a948efd21ca19dd232eff5dd1454df3a0535a
Aruroturo <arturo.alfaro3@my.vcccd.edu>
2024-06-19 16:01:06 -0700

    FINAALLY

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 def main():
-    """
-    ##################################################
-    Comlete your code here
-    Use m_perc=3 , f_perc, nb_perc for your results
-    ##################################################
-    """
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
-male_peeps = int(input("Enter The Number of Male Students: "))
+   # Comlete your code here
+   # Use m_perc=3 , f_perc, nb_perc for your results
+    men_num = int(input("Enter The Number of Male Students: "))
     
-female_peeps = int(input("Enter The Number of Female Students: "))
+    fem_num = int(input("Enter The Number of Female Students: "))
     
-peeps = int(input(" Enter The Number of Non-binary Students: "))
+    nonb_num = int(input(" Enter The Number of Non-binary Students: "))
     
-total_peeps = male_peeps + female_peeps + peeps
+    total_peeps = men_num + fem_num + nonb_num
    
-m_perc = (male_peeps / total_peeps) * 100
+    m_perc = (men_num / total_peeps) * 100
     
-f_perc = (female_peeps / total_peeps) * 100
+    f_perc = (fem_num / total_peeps) * 100
 
-nb_perc = (peeps / total_peeps) * 100
+    nb_perc = (nonb_num / total_peeps) * 100
 
-print(f"The total Number of Students: t\t{total_peeps}")
+    print(f"The total Number of Students: t\t{total_peeps}")
 
-print(f"The Number of Males, Femals and Non-binary: \t{male_peeps} \t\t{female_peeps} \t\{peeps}")
+    print(f"The Number of Males, Femals and Non-binary: \t{men_num} \t{fem_num} \t{nonb_num}")
 
-print(f"The Percentage of Males, Females and Non-Binary \t {male_peeps:.2f}% \t\t{female_peeps:.2f}% \t\t{peeps:.2f}%")
+    print(f"The Percentage of Males, Females and Non-Binary \t {m_perc:.2f}% \t{f_perc:.2f}% \t{nb_perc:.2f}%")
 
 
 if __name__ == '__main__':
